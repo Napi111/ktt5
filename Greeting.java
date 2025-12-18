aff3724f08c8ab896cb2fd6b5ddfc0b961edd94c
napisatomarova <napisatomarova87@gmail.com>
2025-12-18 13:05:59 +0300

    feat: add Greeting.greetColor() for ANSI-colored output

--- Greeting.java
+++ Greeting.java
@@ -2,4 +2,8 @@ public class Greeting {
     public static String greet(String name) {
         return "Hello, " + name + "!";
     }
+    public static String greetColor(String name) {
+        // ANSI green: \u001B[32m, reset: \u001B[0m
+        return "\u001B[32mHello, " + name + "!\u001B[0m";
+    }
 }
